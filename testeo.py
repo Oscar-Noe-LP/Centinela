661e1e1f9d9fb445f891e104b522888318abffa3
Oscar-Noe-LP <debriuwu77@gmail.com>
2025-04-01 11:59:20 -0600

    testing

---
+++ testeo.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def conectar():
+    return sqlite3.connect("centinela.db")
+
+def usuarios():
+    conexion = conectar()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM Usuarios")
+    usuarios = cursor.fetchall()
+    conexion.close()
+    if not usuarios:
+        return ("No hay usuarios registrados")
+    else:
+        return usuarios
+
+
+users = usuarios()
+
+
+if not users:
+    print("No hay usuarios registrados.")
+else:
+    print("Usuarios registrados:")
+    for usuario in users:
+        print(usuario)  
