b27ab75124c56aa2c06afc54e914e6d8d33cbc3c
Oscar-Noe-LP <debriuwu77@gmail.com>
2025-04-29 00:40:16 -0600

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,11 +5,12 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { View, ActivityIndicator } from 'react-native';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+// Evita que se esconda automáticamente la splash screen
+SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -19,22 +20,28 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+      SplashScreen.hideAsync().catch((err) =>
+        console.warn('Error ocultando splash screen:', err)
+      );
     }
   }, [loaded]);
 
   if (!loaded) {
-    return null;
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#1ba098" />
+      </View>
+    );
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="index" options={{title: 'Centinela'}}/>
-        <Stack.Screen name="Login/Login" options={{title: 'Inicio de sesión'}}/> 
-        <Stack.Screen name="Login/Registro" options={{title: 'Registro de usuario'}}/> 
-        <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-        <Stack.Screen name="+not-found" /> 
+        <Stack.Screen name="index" options={{ title: 'Centinela' }} />
+        <Stack.Screen name="Login/Login" options={{ title: 'Inicio de sesión' }} />
+        <Stack.Screen name="Login/Registro" options={{ title: 'Registro de usuario' }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
